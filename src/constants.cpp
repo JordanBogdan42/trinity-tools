3559ee1650dd52159bf4a733b92a82acb1f217f1
JordanBogdan42 <jordanbogdan42@gmail.com>
2024-08-07 15:31:33 -0400

    added namespace std to constants.cpp

--- src/constants.cpp
+++ src/constants.cpp
@@ -1,5 +1,7 @@
 #include "constants.h"
 
+using namespace std;
+
 const string dataDir = "/storage/hive/project/phy-otte/shared/Trinity/Data/";
 const string outDir = "/storage/hive/project/phy-otte/shared/Trinity/DataAnalysis/DataSummary/Output/";
 const int maxCh = 256;
