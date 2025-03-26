eb682f6a883c5717064979f5440089e01053371d
Sofia Stepanoff <sstepanoff3@login-hive-2.pace.gatech.edu>
2025-03-26 12:51:18 -0400

    update to the constant.cpp file for that Musicmap to update mariia changes. 1. made it so that +2 to even numbers 2. all values are -1 so that they match the music plot. -sofia

--- src/constants.cpp
+++ src/constants.cpp
@@ -11,5 +11,5 @@ const int binLen = 120;
 const vector<string> hTitles = {"Average Amplitude of HLED Events [ADC Counts]","Average Amplitude of HLED Events normalized to median","Average Pedestal [ADC Counts]","Average Pedestal RMS [ADC Counts]","Average Amplitude [ADC Counts]","Average Charge [ADC Counts]","Average Peak Time [Time bins]"};
 const vector<string> dTitles = {"Average Amplitude of HLED Events [ADC Counts]","Standard Deviation of Amplitude Distribution","Average Pedestal [ADC Counts]","Average Pedestal RMS [ADC Counts]","Average Amplitude [ADC Counts]","Average Charge [ADC Counts]","Average Peak Time [Time bins]"};
 const vector<double> avgVals = {1100,0.15,3783,26,87,229,249};
-const vector<int> MUSICmap = {2,3,10,11,18,19,26,27,6,7,14,15,22,23,30,31,0,1,8, 9,16,17,24,25,4,5,12,13,20,21,28,29};
+const vector<int> MUSICmap = {3,2,11,10,19,18,27,26,7,6,15,14,23,22,31,30,1,0,9,8,17,16,25,24,5,4,13,12,21,20,29,28};
 const string trStr = "200000ffff250300"; //first 16 bits of 18 bit hex command for setting trigger threshold, last 2 bits are the threshold value
